8d6ef384bb8ce05ba57c92322845343931adbf01
Vlatko Stojkoski <stojvlatko@gmail.com>
2022-02-15 10:48:37 +0100

    update: fix project id

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Badge, Box, Image, Link, Grid } from '@chakra-ui/react';
 
 const Projects = () => {
-	const projectIds = ['1GbLlBKRQO8K_zyNGNOGR_hdmQB3cq0jZXXkmm6YdV3s', '1S9Vs6Egaf_kLrESUAIMyrH3DmKtJ-HRA9X7FLQXVVD'];
+	const projectIds = ['1GbLlBKRQO8K_zyNGNOGR_hdmQB3cq0jZXXkmm6YdV3s', '1S9Vs6Egaf_kLrESUAIMyrH3DmKtJ-HRA9X7FLQXVVDI'];
 
 	return <Grid>
 
@@ -32,24 +32,6 @@ const Projects = () => {
 	<Link href={`https://docs.google.com/presentation/d/${projectIds[1]}/edit?usp=sharing`}>
 		<Box maxW='xs' borderWidth='1px' borderRadius='lg' overflow='hidden'>
 			<Image w='100%' src={`https://lh3.google.com/u/0/d/${projectIds[1]}=w208-h117-iv7`} />
-
-			<Box p='6'>
-				<Box display='flex' alignItems='baseline'>
-					<Badge borderRadius='full' px='2' colorScheme='orange'>
-						History
-					</Badge>
-				</Box>
-
-				<Box
-					mt='1'
-					fontWeight='semibold'
-					as='h4'
-					lineHeight='tight'
-					isTruncated
-				>
-					Охридска Архепископија
-				</Box>
-			</Box>
 
 			<Box p='6'>
 				<Box display='flex' alignItems='baseline'>
