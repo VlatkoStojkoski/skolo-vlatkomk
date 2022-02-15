1783bc4d7ecc0abba9a5527d9a1c85a04023d2a3
Vlatko Stojkoski <stojvlatko@gmail.com>
2022-02-15 11:29:43 +0100

    add: dynamic add projects

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -2,55 +2,42 @@ import React from 'react';
 import { Badge, Box, Image, Link, Grid } from '@chakra-ui/react';
 
 const Projects = () => {
-	const projectIds = ['1GbLlBKRQO8K_zyNGNOGR_hdmQB3cq0jZXXkmm6YdV3s', '1S9Vs6Egaf_kLrESUAIMyrH3DmKtJ-HRA9X7FLQXVVDI'];
+	const projectIds = [{
+		name: 'Ohridska arhepiskopija',
+		id: '1GbLlBKRQO8K_zyNGNOGR_hdmQB3cq0jZXXkmm6YdV3s',
+		class: 'History'
+	}, {
+		name: 'Makedonski proekt',
+		id: '1S9Vs6Egaf_kLrESUAIMyrH3DmKtJ-HRA9X7FLQXVVDI',
+		class: 'Macedonian'
+	}];
 
 	return <Grid w='100%' gap='3' templateColumns='repeat(auto-fit, minmax(300px, 1fr))'>
-	<Link href={`https://docs.google.com/presentation/d/${projectIds[0]}/edit?usp=sharing`}>
-		<Box maxW='xs' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-			<Image w='100%' src={`https://lh3.google.com/u/0/d/${projectIds[0]}`} />
+		{projectIds.map(({name: pName, id: pId, class: pClass}) => 
+			<Link href={`https://docs.google.com/presentation/d/${pId}/edit?usp=sharing`}>
+				<Box maxW='xs' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+					<Image w='100%' src={`https://lh3.google.com/u/0/d/${pId}`} />
 
-			<Box p='6'>
-				<Box display='flex' alignItems='baseline'>
-					<Badge borderRadius='full' px='2' colorScheme='orange'>
-						History
-					</Badge>
-				</Box>
+					<Box p='6'>
+						<Box display='flex' alignItems='baseline'>
+							<Badge borderRadius='full' px='2' colorScheme='orange'>
+								${pClass}
+							</Badge>
+						</Box>
 
-				<Box
-					mt='1'
-					fontWeight='semibold'
-					as='h4'
-					lineHeight='tight'
-					isTruncated
-				>
-					Охридска Архепископија
+						<Box
+							mt='1'
+							fontWeight='semibold'
+							as='h4'
+							lineHeight='tight'
+							isTruncated
+						>
+							${pName}
+						</Box>
+					</Box>
 				</Box>
-			</Box>
-		</Box>
-	</Link>
-	<Link href={`https://docs.google.com/presentation/d/${projectIds[1]}/edit?usp=sharing`}>
-		<Box maxW='xs' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-			<Image w='100%' src={`https://lh3.google.com/u/0/d/${projectIds[1]}`} />
-
-			<Box p='6'>
-				<Box display='flex' alignItems='baseline'>
-					<Badge borderRadius='full' px='2' colorScheme='orange'>
-						Macedonian
-					</Badge>
-				</Box>
-
-				<Box
-					mt='1'
-					fontWeight='semibold'
-					as='h4'
-					lineHeight='tight'
-					isTruncated
-				>
-					Makedonski proekt
-				</Box>
-			</Box>
-		</Box>
-	</Link>
+			</Link>
+		)}
 	</Grid>;
 };
 
